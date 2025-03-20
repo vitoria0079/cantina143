91b400aed672746cf6bb35ee0a4621c2477e3885
vitória0079 <depaulavitoria820@gmail.com>
2025-03-20 10:57:54 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cantina143.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
