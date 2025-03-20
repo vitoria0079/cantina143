b7bdef654145543ef4534634529d29eb96b26327
vitória0079 <depaulavitoria820@gmail.com>
2025-03-20 15:02:12 -0300

    Teste

--- Models/Contexto.cs
+++ Models/Contexto.cs
@@ -1,6 +1,16 @@
-﻿namespace cantina143.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace cantina143.Models
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+        public DbSet<Usuarios> Usuarios { get; set; }
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Fornecedores> Fornecedores { get; set; }
+        public DbSet<Alimentos> Alimentos { get; set; }
     }
 }
